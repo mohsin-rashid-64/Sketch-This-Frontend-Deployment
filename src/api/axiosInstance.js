74f4c6e1b592003abb589c03cd180692479d9c86
Muhammad Mohsin Rashid <m.mohisn@digimarkdevelopers.com>
2025-01-28 16:54:48 +0500

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5000/",
+  baseURL: "https://api.sketch-this.com/",
   // timeout: 5000,
   headers: {
     "Content-Type": "application/json",
